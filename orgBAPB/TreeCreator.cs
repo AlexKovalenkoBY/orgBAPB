3522ca02a06d2b99d77ad57ffb2c75295c236c53
kovalenkoam <kovalenkoam@gmail.com>
2019-05-29 23:33:41 +0300

    added commetns

--- orgBAPB/TreeCreator.cs
+++ orgBAPB/TreeCreator.cs
@@ -9,36 +9,38 @@ namespace orgBAPB
     internal class TreeCreator
     {
         private Dictionary<string, IEnumerable<Podr>> _childPodrsByCodSp;
-
+        /*определили словарь для дочерних СП*/
         internal TreeCreator()
         {
         }
 
-        internal Podr CreateTree(List<Podr> podrs)
+        internal Podr CreateTree(List<Podr> podrs) //метод создания дерева
         {
+            /* подразделения без уникальных номеров и правила их выделения*/
             var podrsWithNonUniqueCodeSp =
                 podrs
                    .GroupBy(p => p.CodeSp)
                    .Where(g => g.Count() > 1)
-                   .Select(g => g.First().CodeSp)
-                   .ToArray();
+                   .Select(g => g.First().CodeSp) 
+                   .ToArray(); //записали в массив
 
             _childPodrsByCodSp =
                 podrs
                     .ToDictionary(p => p.CodeSp, p => podrs.Where(p2 => p2.parentSP == p.CodeSp));
+            /* в словарь записать те СП, у которых код родительского СП = коду текущего СП*/
 
-            Podr root = podrs[0];
+            Podr root = podrs[0]; //выбрали начальное СП
             PopulateChilds(root);
             return root;
         }
 
-        private void PopulateChilds(Podr podr)
+        private void PopulateChilds(Podr podr) //метод построения дерева дочерних СП (структуры банка в цело мначиная от рута)
         {
-            IEnumerable<Podr> childs;
+            IEnumerable<Podr> childs; //список дочерних СП типа podr
             if (_childPodrsByCodSp.TryGetValue(podr.CodeSp, out childs))
-            {
+            { //рекурсия
                 podr.Childs = childs.ToList();
-                foreach (Podr child in podr.Childs)
+                foreach (Podr child in podr.Childs)   //для всех СП из  podr.Childs
                 {
                     PopulateChilds(child);
                 }
